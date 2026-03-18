7be832a61f3517ce50476c192620690d1cffe13b
MatiasTanoni <matitanoni@gmail.com>
2026-03-18 19:03:17 -0300

    bien

--- front/src/app/components/navbar/navbar.ts
+++ front/src/app/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, TitleCasePipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Movements } from '../../components/movements/movements';
-
+import { AuthService } from '../../services/auth-service';
+import { TitleCasePipe } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [Movements],
+  imports: [Movements, TitleCasePipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  authService = inject(AuthService);
   isModalOpen: boolean = false;
 
   abrirModal() {
